6ae13ffddef970df49b3ff7fd64ae92566a7340a
Drymis <50848150+Drymis@users.noreply.github.com>
2021-11-13 17:25:18 +0100

    improved error messages on the app and added try again button

--- intelistyle-app/src/App.js
+++ intelistyle-app/src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
     super(props);
     this.itemsPerPage = 10;
     this.state = {
-      garmentsToDisplay: [],
+      garmentsToDisplay: null,
       pageIndex: 0
     };
     this.search = debounce((text) => {
@@ -25,34 +25,55 @@ class App extends React.Component {
   }
 
   render() {
-    const garments = [];
-    this.pagesNumber = Math.floor(this.state.garmentsToDisplay.length / this.itemsPerPage);
-    if (!(this.state.garmentsToDisplay.length % this.itemsPerPage)) {
-      this.pagesNumber--;
+    let garments;
+    if (this.state.garmentsToDisplay) {
+      garments = [];
+      this.pagesNumber = Math.floor(this.state.garmentsToDisplay.length / this.itemsPerPage);
+      if (!(this.state.garmentsToDisplay.length % this.itemsPerPage) && this.pagesNumber) {
+        this.pagesNumber--;
+      }
+      const garmentsListStart = this.state.pageIndex * this.itemsPerPage;
+      let garmentsListEnd = garmentsListStart + this.itemsPerPage;
+      if (garmentsListEnd > this.state.garmentsToDisplay.length) {
+        garmentsListEnd = this.state.garmentsToDisplay.length;
+      }
+      const garmentsList = this.state.garmentsToDisplay.slice(garmentsListStart, garmentsListEnd);
+      garmentsList.forEach((garment, garmentIndex) => {
+        garments.push(
+          <Garment
+            key={garmentIndex}
+            title={garment.product_title}
+            description={garment.product_description}
+            gender={garment.gender}
+            stock={garment.stock}
+            brand={garment.brand}
+            price={garment.price}
+            currency={garment.currency_code}
+            categories={garment.product_categories}
+            // images={garment.images}
+            url={garment.url}
+          />
+        );
+      });
     }
-    const garmentsListStart = this.state.pageIndex * this.itemsPerPage;
-    let garmentsListEnd = garmentsListStart + this.itemsPerPage;
-    if (garmentsListEnd > this.state.garmentsToDisplay.length) {
-      garmentsListEnd = this.state.garmentsToDisplay.length;
+
+    let appContent;
+    if (this.state.error) {
+      appContent = 
+        <div className="app app-error">
+          <div>There has been an issue</div>
+          <div className="navigation-button" onClick={event => this.trySearchAgain()}>try again</div>
+        </div>;
+    } else {
+      if (!garments) {
+        appContent = <div className="app app-loading">Loading results...</div>;
+      } else if (!garments?.length) {
+        appContent = <div className="app app-message">No results</div>;
+      } else {
+        appContent = <div className="app">{garments}</div>;
+      }
     }
-    const garmentsList = this.state.garmentsToDisplay.slice(garmentsListStart, garmentsListEnd);
-    garmentsList.forEach((garment, garmentIndex) => {
-      garments.push(
-        <Garment
-          key={garmentIndex}
-          title={garment.product_title}
-          description={garment.product_description}
-          gender={garment.gender}
-          stock={garment.stock}
-          brand={garment.brand}
-          price={garment.price}
-          currency={garment.currency_code}
-          categories={garment.product_categories}
-          // images={garment.images}
-          url={garment.url}
-        />
-      );
-    });
+
     return (
       <div className="page-container">
 
@@ -64,7 +85,7 @@ class App extends React.Component {
           <div className="navigation-button" onClick={event => this.changePage(true)}>Next Page</div>
         </div>
 
-        <div className="app">{garments}</div>
+        {appContent}
       </div>
     );
   }
@@ -75,7 +96,7 @@ class App extends React.Component {
       queryParams += `?search=${search}`;
     }
     return fetch(
-      `http://localhost:3001/garment${queryParams}`,
+      `${process.env.REACT_APP_API_HOST}/garment${queryParams}`,
       {
         method: "GET",
         headers: new Headers({
@@ -84,7 +105,12 @@ class App extends React.Component {
       }
     )
       .then(res => res.json())
-      .catch(error => console.log(error));
+      .catch(error => {this.setState({error}); console.log(error);});
+  }
+
+  trySearchAgain() {
+    this.setState({error: null, garmentsToDisplay: null})
+    this.search();
   }
 
   updateGarments(result) {
@@ -95,8 +121,12 @@ class App extends React.Component {
   }
 
   fetchAndUpdateGarments(search = undefined) {
+    this.setState({
+      garmentsToDisplay: null,
+      pageIndex: 0
+    });
     this.fetchGarments(search).then((response) => {
-      this.updateGarments(response); 
+      this.updateGarments(response);
     });
   }
 
